fc3c744b9692f58de861d602df1495947e7977a3
mohammadjavad arab <mohamadjavadarab92@gmail.com>
2024-11-04 15:40:10 +0400

    ESP_Sendig_data

---
+++ Python Codes/SendDatatoESP.py
@@ -0,0 +1,60 @@
+import paho.mqtt.client as mqtt
+import json
+import ssl
+import time
+import _thread
+
+
+# Defining on_connect function
+##### On Connect
+def on_connect(client, userdata, flags, rc):
+    print("Connected to AWS IoT:" + str(rc))
+
+### on Subscribe message
+def on_message(client, userdata, msg):
+    print(f"Message received on topic {msg.topic}")
+    payload = json.loads(msg.payload.decode('utf-8'))
+    print(f"Payload: {payload}")
+# Defining client
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+
+# cerificate
+client.tls_set(ca_certs='D:\linux\IoT AWS\python_certificate\AmazonRootCA1.pem', certfile='D:\linux\IoT AWS\python_certificate\certificate.pem.crt', keyfile='D:\linux\IoT AWS\python_certificate\private_key.pem.key', tls_version=ssl.PROTOCOL_SSLv23)
+client.tls_insecure_set(True)
+client.connect("ak40v1wcytj5n-ats.iot.ap-southeast-2.amazonaws.com",8883,60)
+
+def publishData(txt):
+    print(txt)
+    ctr = 1
+    lamp1 = True
+    lamp2 = True
+    while (True):
+        if lamp1 == True:
+            msg = "ON1"
+            lamp1 = False
+        else:
+            msg = "OFF1"
+            lamp1 = True
+        print(msg)
+        client.publish("py/pub", payload=msg, qos=0, retain=False)
+        time.sleep(2)
+        if lamp2 == True:
+            msg = "ON2"
+            lamp2 = False
+        else:
+            msg = "OFF2"
+            lamp2 = True
+        print(msg)
+        client.publish("py/pub", payload=msg, qos=0, retain=False)
+        ctr = ctr + 1
+        time.sleep(2)
+
+# Create a new thread to publish data
+_thread.start_new_thread(publishData,("Spin-up new Thread...",))
+
+# loop publishing data forever and you can not stop it, Just terminate the running
+client.loop_forever()
